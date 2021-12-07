046d51db4da2e6129d852362382371da48526ed4
lpofredc <frederic.cloitre@lpo.fr>
2021-12-07 11:14:06 +0100

    init typescript

---
+++ build/index.js
@@ -0,0 +1,5 @@
+console.log("hello");
+var myNum = 1;
+var myBool = true;
+var myString = 'tete';
+var myArray = ['1', 'b'];
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("hello");
+const myNum: number = 1;
+const myBool: boolean = true;
+const myString: string = 'tete';
+const myArray: string[] = ['1', 'b'];
